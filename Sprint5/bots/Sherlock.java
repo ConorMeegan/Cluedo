4f958114d7548b0b360690330c1a618df08e512d
Mason Smith <masonsmith40@hotmail.co.uk>
2018-04-25 00:05:00 +0100

    ....

--- Sprint5/bots/Sherlock.java
+++ Sprint5/bots/Sherlock.java
@@ -1,28 +1,35 @@
 package bots;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Random;
 
 import gameengine.*;
 
 public class Sherlock implements BotAPI {
 
+	String[] allCards = {"Plum", "White", "Scarlett", "Green", "Mustard", "Peacock"
+			,"Rope", "Dagger", "Wrench", "Pistol", "Candlestick", "Lead Pipe","Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library",
+            "Study", "Hall", "Lounge", "Dining Room", "Cellar"};
+	ArrayList<String> AllCards = new ArrayList<String>();
+	
 	ArrayList<String> knownPlayers = new ArrayList<>();
 	ArrayList<String> suspectPlayers = new ArrayList<>();
+	
 	ArrayList<String> knownWeapons = new ArrayList<>();
 	ArrayList<String> suspectWeapons = new ArrayList<>();
+	
 	ArrayList<String> knownRooms = new ArrayList<>();
 	ArrayList<String> suspectRooms = new ArrayList<>();
 	
 	int lastIndex = 0;
+	int cardsEach;
+	
 	
 	private int turnsDone = 0;
-	private String target = "Billard Room";
 
-	ArrayList<Coordinates> bestPath = new ArrayList<Coordinates>();
-	Coordinates checkedCords;
-	
-	int lowest = 100;
+	private String target = "Lounge";
 	
 	// The public API of Bot must not change
 	// This is ONLY class that you can edit in the program
@@ -38,7 +45,7 @@ public class Sherlock implements BotAPI {
 	private Deck deck;
 	
 	String peacockToConservatory = "llllluu";
-	String peacockToBilliardRoom = "llllllddr";
+	String peacockToBilliardRoom = "lllllldddr";
 	String peacockToLibrary = "lllllldddddddrrrd";
 	String peacockToStudy = "lllllllddddddddddddddrd";
 	String peacockToBallRoom = "lllllllul";
@@ -58,27 +65,27 @@ public class Sherlock implements BotAPI {
 	String mustardToKitchen = "rrrrrrrruuuuuuuuulllluu";
 	
 	String plumToStudy = "lllllldd";
-	String plumToHall = "llllllllddl";
+	String plumToHall = "lllllllldl";
 	String plumToLounge = "lllllllluullllllllldd";
-	String plumToDiningRoom = "lllllllluulllllllllluu";
+	String plumToDiningRoom = "lllllllluullllllllluu";
 	String plumToLibrary = "llllllluuur";
-	String plumToBilliardRoom = "llllllluuuuuuuuuur";
+	String plumToBilliardRoom = "llllllluuuuuuuuuurr";
 	String plumToConservatory = "llllllluuuuuuuuuuuuuurru";
 	String plumToBallRoom = "llllllluuuuuuuuuuuuuul";
-	String plumToKitchen = "lllllllluulllllllluuuuuuuuulllluu";
+	String plumToKitchen = "lllllllluullllllluuuuuuuuulllluu";
 	
 	String whiteToBallRoom = "dllddddr";
 	String whiteToKitchen= "dllddddddlllu";
 	String whiteToConservatory = "dlldddddddrrrrrrrrrruuuru";
 	String whiteToDiningRoom = "dllddddddddrdddl";
-	String whiteToBilliardRoom = "dlldddddddrrrrrrrrrrdl";
-	String whiteToLibrary = "dlldddddddrrrrrrrrrddddddddl";
+	String whiteToBilliardRoom = "dllddddddddrrrrrrrrrrr";
+	String whiteToLibrary = "dllddddddddrrrrrrrrddddrrrrrd";
 	String whiteToLounge = "dllddddddddrdddddddddlld";
 	String whiteToHall = "dllddddddddrddddddddrrrd";
 	String whiteToStudy= "dlldddddddrrrrrrrrrddddddddddddrd";
 	
 	//Green
-	String greenToBallRoom = "drrdddl";
+	String greenToBallRoom = "drrddddl";
 	String greenToConservatory = "drrdrdddru";
 	String greenToBilliardRoom = "drrddddddddrr";
 	String greenToLibrary = "drrdddddddddddddddr";
@@ -89,14 +96,14 @@ public class Sherlock implements BotAPI {
 	String greenToLounge = "drrddddddddlllllllldddddddddlld";
 	
 	//Scarlet
-	String scarlettToLounge = "uuuuuuuld";
+	String scarlettToLounge = "uuuuuuld";
 	String scarlettToDiningRoom = "uuuuuuuluuu";
-	String scarlettToHall = "uuuuuuuurrrrd";
-	String scarlettToStudy = "uuuuuuuurrrrrrrrrdddrd";
-	String scarlettToLibrary = "uuuuuuuurrrrrrrrrur";
+	String scarlettToHall = "uuuuuuurrrrd";
+	String scarlettToStudy = "uuuuuuurrrrrrrrrdddrd";
+	String scarlettToLibrary = "uuuuuuurrrrrrrrrur";
 	String scarlettToBallRoom = "uuuuuuuuruuuuuuuuuuru";
 	String scarlettToKitchen = "uuuuuuuuruuuuuuuulllluu";
-	String scarlettToConservatory = "uuuuuuuuruuuuuuuuuurrrrrrrrruuulu";
+	String scarlettToConservatory = "uuuuuuuuruuuuuuuurrrrrrrrruuuru";
 	String scarlettToBilliardRoom = "uuuuuuurrrrrrrrruuuuuuuurr";
 
 	public Sherlock(Player player, PlayersInfo playersInfo, Map map, Dice dice, Log log, Deck deck) {
@@ -106,9 +113,8 @@ public class Sherlock implements BotAPI {
 		this.dice = dice;
 		this.log = log;
 		this.deck = deck;
-
 	}
-
+	
 	public String getName() {
 		return "Sherlock"; // must match the class name
 	}
@@ -131,7 +137,9 @@ public class Sherlock implements BotAPI {
 
 	public String getMove() {
 
-		System.out.println(player.getToken().getName());
+		
+		
+		
 
 		if(player.getToken().getName().equals("Mustard")){
 			if(target.equals("Lounge")) {
@@ -164,7 +172,7 @@ public class Sherlock implements BotAPI {
 				lastIndex += 1;
 				turnsDone += 1;
 				return Character.toString(toReturn);
-			}else if(target.equals("Billard Room")) {
+			}else if(target.equals("Billiard Room")) {
 				
 				char toReturn =  mustardToBilliardRoom.charAt(lastIndex);
 				lastIndex += 1;
@@ -220,7 +228,7 @@ public class Sherlock implements BotAPI {
 				lastIndex += 1;
 				turnsDone += 1;
 				return Character.toString(toReturn);
-			}else if(target.equals("Billard Room")) {
+			}else if(target.equals("Billiard Room")) {
 				
 				char toReturn =  scarlettToBilliardRoom.charAt(lastIndex);
 				lastIndex += 1;
@@ -276,7 +284,7 @@ public class Sherlock implements BotAPI {
 				lastIndex += 1;
 				turnsDone += 1;
 				return Character.toString(toReturn);
-			}else if(target.equals("Billard Room")) {
+			}else if(target.equals("Billiard Room")) {
 				
 				char toReturn =  whiteToBilliardRoom.charAt(lastIndex);
 				lastIndex += 1;
@@ -332,7 +340,7 @@ public class Sherlock implements BotAPI {
 				lastIndex += 1;
 				turnsDone += 1;
 				return Character.toString(toReturn);
-			}else if(target.equals("Billard Room")) {
+			}else if(target.equals("Billiard Room")) {
 				
 				char toReturn =  greenToBilliardRoom.charAt(lastIndex);
 				lastIndex += 1;
@@ -388,7 +396,7 @@ public class Sherlock implements BotAPI {
 				lastIndex += 1;
 				turnsDone += 1;
 				return Character.toString(toReturn);
-			}else if(target.equals("Billard Room")) {
+			}else if(target.equals("Billiard Room")) {
 				
 				char toReturn =  plumToBilliardRoom.charAt(lastIndex);
 				lastIndex += 1;
@@ -444,7 +452,7 @@ public class Sherlock implements BotAPI {
 				lastIndex += 1;
 				turnsDone += 1;
 				return Character.toString(toReturn);
-			}else if(target.equals("Billard Room")) {
+			}else if(target.equals("Billiard Room")) {
 				
 				char toReturn =  peacockToBilliardRoom.charAt(lastIndex);
 				lastIndex += 1;
@@ -507,12 +515,12 @@ public class Sherlock implements BotAPI {
 				suspectPlayers.add(Names.SUSPECT_NAMES[i]);
 			}
 		}
-		System.out.println(player.getCards());
-		System.out.println(knownPlayers);
-		System.out.println(suspectPlayers);
+		//System.out.println(player.getCards());
+		//System.out.println(knownPlayers);
+		//System.out.println(suspectPlayers);
 
 
-		return suspectPlayers.get(0);
+		return suspectPlayers.get(1);
 	}
 
 	public String getWeapon() {
@@ -547,9 +555,9 @@ public class Sherlock implements BotAPI {
 				suspectWeapons.add(Names.WEAPON_NAMES[i]);
 			}
 		}
-		System.out.println(player.getCards());
-		System.out.println(knownWeapons);
-		System.out.println(suspectWeapons);
+		//System.out.println(player.getCards());
+		//System.out.println(knownWeapons);
+		//System.out.println(suspectWeapons);
 
 		return suspectWeapons.get(0);
 	}
@@ -596,9 +604,9 @@ public class Sherlock implements BotAPI {
 			}
 
 		}
-		System.out.println(player.getCards());
-		System.out.println(knownRooms);
-		System.out.println(suspectRooms);
+		//System.out.println(player.getCards());
+		//System.out.println(knownRooms);
+		//System.out.println(suspectRooms);
 
 		return suspectRooms.get(0);
 	}
@@ -625,5 +633,23 @@ public class Sherlock implements BotAPI {
 	public void notifyResponse(Log response) {
 		// Add your code here
 
+	}
+	
+	public void Initialize() {
+		
+		for(int i =0;i<AllCards.size();i++) {
+			AllCards.add(allCards[i]);
+			System.out.println(allCards[i]);
+		}
+		System.out.println(AllCards.size());
+		cardsEach = 18/playersInfo.numPlayers();
+		
+		
+			//System.out.println(player.getCards().contains("Ball Room"));
+			
+		
+		if(cardsEach > 0) {
+			
+		}
 	}
 }
