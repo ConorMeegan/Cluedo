dcef2fd9bcfceaafc03c45f836c16047cd97159e
ConorMeegan <conmeegan@gmail.com>
2018-04-24 17:32:14 +0100

    Stupid hardcoding part 1!

--- Sprint5/src/bots/Sherlock.java
+++ Sprint5/src/bots/Sherlock.java
@@ -65,7 +65,7 @@ public class Sherlock implements BotAPI {
 	
 	private int turnsDone = 0;
 	private Coordinates target = new Coordinates(19,6);
-	
+
 	ArrayList<Coordinates> bestPath = new ArrayList<Coordinates>();
 	Coordinates checkedCords;
 	public node[][] mapNodes = new node[24][24];
@@ -123,62 +123,30 @@ public class Sherlock implements BotAPI {
 	}
 
 	public String getMove() {
-		String move;
-		int targetX = target.getCol();
-		int targetY = target.getRow();
-		int playerX = player.getToken().getPosition().getCol();
-		int playerY = player.getToken().getPosition().getRow();
-		//int distOfNodeAndTarget = distance(playerX,playerY,targetX,targetY);
-		
-		int xDifference = playerX-targetX;
-		int yDifference = playerY-targetY;
-		
-		System.out.println("xDifference:" +xDifference);
-		System.out.println("yDifference:" +yDifference);
-		
-		if(yDifference < -1) {
-			if(map.isCorridor(new Coordinates(playerX ,playerY+1))) {
-				turnsDone += 1;
-				System.out.println("true for D");
-				return "d";
-			}else {
-				if(xDifference < -1 && map.isCorridor(new Coordinates(playerX +1,playerY))) {
-					turnsDone += 1;
-					System.out.println("true for D and R");
-					return "r";
-				}else if(xDifference > -1 && map.isCorridor(new Coordinates(playerX -1,playerY))) {
-					turnsDone += 1;
-					System.out.println("true for D and L");
-					return "l";
-				}
-			}
-		}else if(yDifference > -1) {
-			if(map.isCorridor(new Coordinates(playerX ,playerY-1))) {
-				turnsDone += 1;
-				System.out.println("true for D");
-				return "u";
-			}else {
-				if(xDifference < -1 && map.isCorridor(new Coordinates(playerX +1,playerY))) {
-					turnsDone += 1;
-					System.out.println("true for D and R");
-					return "r";
-				}else if(xDifference > -1 && map.isCorridor(new Coordinates(playerX -1,playerY))) {
-					turnsDone += 1;
-					System.out.println("true for D and L");
-					return "l";
-				}
-			}
-		}else {
-			//left and right stuff
-			if(xDifference < -1) {
-				turnsDone += 1;
-				return "r";
-			}else if(xDifference > -1) {
-				turnsDone += 1;
-				return "l";
-			}
-		}
-		turnsDone += 1;
+
+		String peacockToConservatory = "llllluu";
+		String peacockToBilliardRoom = "llllllddr";
+		String peacockToLibrary = "lllllldddddddrrrd";
+		String peacockToStudy = "lllllllddddddddddddddrd";
+		String peacockToBallRoom = "lllllllul";
+		String peacockToKitchen = "lllllllddlllllllllllluu";
+		String peacockToDiningRoom = "llllllldddlllllllldddl";
+		String peacockToHall = "llllllldddddddddddlllld";
+		String peacockToLounge = "llllllldddddddddddlllllllllldd";
+
+		String mustardToLounge = "rrrrrrdd";
+		String mustardToDiningRoom = "rrrrrruu";
+		String mustardToHall = "rrrrrrrrrrrd";
+		String mustardToStudy = "rrrrrrrrrrrrrrrrdddrd";
+		String mustardToLibrary = "rrrrrrrrrrrrrrrrur";
+		String mustardToBilliardRoom = "rrrrrrrrrrrrrrrruuuuuuuurr";
+		String mustardToConservatory = "rrrrrrrrrrrrrrrruuuuuuuuuuuurru";
+		String mustardToBallRoom = "rrrrrrrruuuuuuuuuru";
+		String mustardToKitchen = "rrrrrrrruuuuuuuuulllluu";
+
+		//String conservatoryToBallRoom = "lll";
+
+
 		return "u";
 	}
 
@@ -334,84 +302,5 @@ public class Sherlock implements BotAPI {
 		// Add your code here
 
 	}
-	
-	public int distance(int x,int y, int x2, int y2) {
-		
-		return (int) Math.sqrt(Math.pow((x2 - x), 2) + Math.pow((y2 - y), 2));
-	}
-	
-	public void getMove(int x, int y, int targetX, int targetY,int l) throws ArrayIndexOutOfBoundsException{
-
-
-		int lowestX = 0;
-		int lowestY = 0;
-		
-		
-		if(x != targetX && y != targetY) {
-			if(map.isCorridor(new Coordinates(x-1,y))) {
-				//up
-				mapNodes[x-1][y].setD(distance(x,y,x-1,y));
-				System.out.println("up Node(D):" +distance(x,y,x-1,y));
-				mapNodes[x-1][y].setH(distance(x-1,y,targetX,targetY));
-				System.out.println("Up Node(H):" +distance(x-1,y,targetX,targetY));
-				mapNodes[x-1][y].setT(mapNodes[x-1][y].getD() + mapNodes[x-1][y].getH());
-				System.out.println("Up Node(T):"+mapNodes[x-1][y].getT());
-				
-			}
-			if(map.isCorridor(new Coordinates(x+1,y))) {
-				//down
-				mapNodes[x+1][y].setD(distance(x,y,x+1,y));
-				System.out.println("Down Node(D):" +distance(x,y,x+1,y));
-				mapNodes[x+1][y].setH(distance(x+1,y,targetX,targetY));
-				System.out.println("Down Node(H):" +distance(x+1,y,targetX,targetY));
-				mapNodes[x+1][y].setT(mapNodes[x+1][y].getD() + mapNodes[x+1][y].getH());
-				System.out.println("Down Node(T):" +mapNodes[x+1][y].getT());
-				
-			}
-			if(map.isCorridor(new Coordinates(x,y+1))) {
-				//right
-				mapNodes[x][y+1].setD(distance(x,y,x,y+1));
-				System.out.println("Right Node(D):" +distance(x,y,x,y+1));
-				mapNodes[x][y+1].setH(distance(x,y+1,targetX,targetY));
-				System.out.println("Right Node(H):" +distance(x,y+1,targetX,targetY));
-				mapNodes[x][y+1].setT(mapNodes[x][y+1].getD() + mapNodes[x][y+1].getH());
-				System.out.println("Right Node(T):" +mapNodes[x][y+1].getT());
-				
-			}
-			if(map.isCorridor(new Coordinates(x,y-1))) {
-				//left
-				mapNodes[x][y-1].setD(distance(x,y,x,y-1));
-				System.out.println("Left Node(D):" +distance(x,y,x,y-1));
-				mapNodes[x][y-1].setH(distance(x,y-1,targetX,targetY));
-				System.out.println("Left Node(H):" +distance(x,y-1,targetX,targetY));
-				mapNodes[x][y-1].setT(mapNodes[x][y-1].getD() + mapNodes[x][y-1].getH());
-				System.out.println("Left Node(T):" +mapNodes[x][y-1].getT());
-			}
-			
-			
-			for(int i=0;i<24;i++) {
-				for(int j=0;j<24;j++) {
-					if(mapNodes[i][j].getT() < l && mapNodes[i][j].getT() != -1) {
-						lowest = mapNodes[i][j].getT();
-						lowestX = i;
-						lowestY = j;
-
-					}
-				}
-			}
-			checkedCords=new Coordinates(lowestX,lowestY);
-			System.out.println();
-			System.out.println(checkedCords.toString());
-			for(int i=0;i<24;i++) {
-				for(int j=0;j<24;j++) {
-					System.out.printf("%d ",mapNodes[i][j].getT());
-				}
-				System.out.println();
-			}
-		}else return;
-		
-	}
-
-
 
 }
